c9a479bea784846b677fd2faf8e5c77b1da70c95
Prakit Si <4016687+asynchronos@users.noreply.github.com>
2020-11-24 12:34:24 +0700

    add cognitive code from leang, but can not use FileStorage to service.

--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 https://code.visualstudio.com/docs/python/python-tutorial
 https://code.visualstudio.com/docs/languages/python
 https://docs.microsoft.com/en-us/azure/azure-functions/create-first-function-vs-code-csharp?pivots=programming-language-python
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-add-output-binding-storage-queue-vs-code?pivots=programming-language-python
--- speech2text/__init__.py
+++ speech2text/__init__.py
@@ -4,7 +4,6 @@ from mimetypes import MimeTypes
 import azure.functions as func
 import mimetypes
 
-
 def main(req: func.HttpRequest,context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -33,12 +32,76 @@ def main(req: func.HttpRequest,context: func.Context) -> func.HttpResponse:
                 ,status_code=200
             )
 
+def read_file(file):
+    return f"{file.filename} uploaded." + f"\n\nContent: \n{file.read()}"
+
 def leang_ai(file):
     # add your code here 
-    
+    import azure.cognitiveservices.speech as speechsdk
+    import time
+    import datetime
+
+    # Creates an instance of a speech config with specified subscription key and service region.
+    # Replace with your own subscription key and region identifier from here: https://aka.ms/speech/sdkregion
+    speech_key, service_region = "53670c8c8ad14415863b7206bbccb48c", "eastus"
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+    # Creates an audio configuration that points to an audio file.
+    # Replace with your own audio filename.
+    audio_filename = "see_you_again.wav"
+    audio_input = speechsdk.audio.AudioConfig(filename=audio_filename)
+
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    audio_config = speechsdk.audio.AudioConfig(filename=audio_input)
+
+    # Creates a speech recognizer using a file as audio input, also specify the speech language
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config, audio_config)
+
+    # Creates a recognizer with the given settings
+    speech_config.speech_recognition_language="en-US"
+    speech_config.request_word_level_timestamps()
+    speech_config.enable_dictation()
+    speech_config.output_format = speechsdk.OutputFormat(1)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+    #result = speech_recognizer.recognize_once()
+    all_results = []
+
+    #https://docs.microsoft.com/en-us/python/api/azure-cognitiveservices-speech/azure.cognitiveservices.speech.recognitionresult?view=azure-python
+    def handle_final_result(evt):
+        all_results.append(evt.result.text+"\n") 
+        
+    done = False
+
+    def stop_cb(evt):
+        print('CLOSING on {}'.format(evt))
+        speech_recognizer.stop_continuous_recognition()
+        global done
+        done= True
+
+    #Appends the recognized text to the all_results variable. 
+    speech_recognizer.recognized.connect(handle_final_result) 
+
+    #Connect callbacks to the events fired by the speech recognizer & displays the info/status
+    #Ref:https://docs.microsoft.com/en-us/python/api/azure-cognitiveservices-speech/azure.cognitiveservices.speech.eventsignal?view=azure-python   
+    speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+    speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous recognition on either session stopped or canceled events
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+
+    speech_recognizer.start_continuous_recognition()
+
+    while not done:
+        time.sleep(.5)
 
     speechText = "Your Result."
     return speechText
 
 def az_speech_regconite():
-    return leang_ai
+    return read_file
+    #return leang_ai
